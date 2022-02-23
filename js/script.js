c1f5a69c0a95d70cd46dc63ab375972b4a29e009
gatsuBerserk <berserkdev@outlook.it>
2022-02-23 16:55:15 +0100

    take user data and print it to the console

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-const firstLastName; 
-const km; 
+const sendingButtom = document.getElementById("sending"); 
+const cancelButtom = document.getElementById("cancel"); 
+
+sendingButtom.addEventListener("click", function(){
+    const infoUser = document.getElementById("name-user").value; 
+    console.log(infoUser);
+    const kmTrip =document.getElementById("km-trip").value;
+    console.log(kmTrip); 
+    const age = document.getElementById("age").value;
+    console.log(age); 
+    let costTrip= (kmTrip * 0.27).toFixed(2);
+    console.log(costTrip + "€");
+})
